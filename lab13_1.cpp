e33c8c852c2a6df564288ecc3ea5ac662693459f
nnapatsiri@gmail.com <nnapatsiri@gmail.com>
2021-12-24 13:55:54 +0700

    lab13_1.cpp

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,24 @@
 #include<iostream>
+#include <ctime>
 using namespace std;
+
+int fibonacci(int x);
 
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int x){
+    int y;
+    if(x==0)
+    cout << 0;
+    if(x==1)
+    cout << 1;
+    if(x>1)
+    y = fibonacci(x-1) + fibonacci(x-2);
+    
+    return y;
 }
